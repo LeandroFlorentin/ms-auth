d03188ecec4fa196f3e8fc6853be8524f5448203
LeandroFlorentin <leandro.florentin@hotmail.com>
2025-06-27 22:04:06 -0300

    winston

--- interfaces/controllers/auth.controller.ts
+++ interfaces/controllers/auth.controller.ts
@@ -3,11 +3,17 @@ import { userRepository } from '&/infrastructure/database/repositories/user.repo
 import { ILoginBody } from '&/application/dtos/auth/login.dto';
 import { meUser, loginUser } from '&/application/use-cases/auth';
 import { RequestWithToken } from '&/types/express';
+import buildLogger from '&/infrastructure/winston';
+
+const service = 'auth';
+
+const logger = buildLogger(service);
 
 export const loginUserHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const body = req.body as ILoginBody;
     const token = await loginUser(userRepository, body);
+    logger.log(token);
     res.status(200).json({ token });
   } catch (error) {
     next(error);
@@ -18,6 +24,7 @@ export const meUserHandler = async (req: RequestWithToken, res: Response, next: 
   try {
     const token = req.token!;
     const decoded = meUser(token);
+    logger.log(decoded);
     res.status(200).json(decoded);
   } catch (error) {
     next(error);
--- interfaces/middlewares/error.middleware.ts
+++ interfaces/middlewares/error.middleware.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import { ZodError, manageMessageZod } from '&/infrastructure/zod/index';
+import buildLogger from '&/infrastructure/winston';
+
+const logger = buildLogger('middlewareError');
 
 const jwtErrorNames = ['TokenExpiredError', 'JsonWebTokenError', 'NotBeforeError'];
 
 export const errorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+
   if (jwtErrorNames.includes(err.name)) {
     res.status(401).json({
       errors: [err.message],
