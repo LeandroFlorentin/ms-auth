a1f734ca4d676c5cefc5da75e2be262db3c69a57
LeandroFlorentin <leandro.florentin@hotmail.com>
2025-07-08 22:31:10 -0300

    redis-to-lower-case

--- infrastructure/cache/repositories/index.ts
+++ infrastructure/cache/repositories/index.ts
@@ -2,9 +2,9 @@ import cache from '&/infrastructure/cache';
 import { IUserDB } from '&/domain/cache/cache.types';
 
 export const cacheRepository = {
-  setValue: async (key: string, value: string): Promise<string | null> => await cache.set(key, value),
+  setValue: async (key: string, value: string): Promise<string | null> => await cache.set(key.toLowerCase(), value),
   getValue: async (key: string): Promise<IUserDB | null> => {
-    const user = await cache.get(key);
+    const user = await cache.get(key.toLowerCase());
     return user ? (JSON.parse(user) as IUserDB) : null;
   },
 };
